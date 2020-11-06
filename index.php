c7ac80182ebaf0f27d25ecb61024d0acfc41ef9e
Sorachat173 <73683414+Sorachat173@users.noreply.github.com>
2020-11-06 17:54:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,30 +4,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>lab01</title>
+    <title>Lab2</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/c
-    ss/bootstrap.min.css" integrity="sha384-
-    TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="a
-    nonymous">
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class=" col-sm-6">
-                <img src="img/cat01.jpg"style="height: 99.5%; width: 100%; class=img-fluid;" />
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/black1.jpg" width=250px height="200px" >
+                    <div class="card-body">
+                        <h4 class="card-title">Flower</h4>
+                        <p class="card-text" style="color: #a9a9a9;">Lorem ipsum dolor sit amet consectetur !<p>
+                        <div class="row" style="margin: 0.2rem;">
+                            <img class="profile-pict" src="img/profile2.jpg" width=32px height="32px">
+                            <div class="col" style="line-height: 0.3rem;">
+                                <p style="color: black; font-weight: bold;">Sorachat Sungkhapan</p>
+                                <p style="color: #a9a9a9;">6 November 2020</p>
+                            </div>
+                            <img class="like-button" src="img/heart.jpg" width=45px height="45px">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class=" col-sm-3">
-                <img src="img/cat01.jpg" class="img-fluid" style="padding: 5px;"/>
-                <img src="img/cat01.jpg" class="img-fluid" style="padding: 5px;"/>
-                <img src="img/cat01.jpg" class="img-fluid" style="padding: 5px;"/>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/balck2.jpg" width=250px height="200px" >
+                    <div class="card-body">
+                        <h4 class="card-title">Road</h4>
+                        <p class="card-text" style="color: #a9a9a9;">Lorem ipsum dolor sit amet consectetur !<p>
+                        <div class="row" style="margin: 0.2rem;">
+                            <img class="profile-pict" src="img/profile2.jpg" width=32px height="32px">
+                            <div class="col" style="line-height: 0.3rem;">
+                                <p style="color: black; font-weight: bold;">Sorachat Sungkhapan</p>
+                                <p style="color: #a9a9a9;">6 November 2020</p>
+                            </div>
+                            <img class="like-button" src="img/heart.jpg" width=45px height="45px">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-sm-3">
-                <img src="img/cat01.jpg"class="img-fluid" style="height: 67%;width: 100%; padding: 5px;"/>
-                <img class="img-fluid" src="img/cat01.jpg"style="height: 33%;width: 100%; padding: 5px;"/>
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/balck3.jpg" width=250px height="200px" >
+                    <div class="card-body">
+                        <h4 class="card-title">Smile</h4>
+                        <p class="card-text" style="color: #a9a9a9;">Lorem ipsum dolor sit amet consectetur !<p>
+                        <div class="row" style="margin: 0.2rem;">
+                            <img class="profile-pict" src="img/profile2.jpg" width=32px height="32px">
+                            <div class="col" style="line-height: 0.3rem;">
+                                <p style="color: black; font-weight: bold;">Sorachat Sungkhapan</p>
+                                <p style="color: #a9a9a9;">6 November 2020</p>
+                            </div>
+                            <img class="like-button" src="img/heart.jpg" width=45px height="45px">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <a href="/index.php" class="btn btn-danger">Back</a>
     </div>
-
 </body>
 </html>
